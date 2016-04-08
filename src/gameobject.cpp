a88d9625e9578a95d80eab694036c432525d587e
climaco02 <climaco.cafe@gmail.com>
2016-04-08 05:53:30 -0300

    Adding description to attributes and methods of the GameObject class

--- src/gameobject.cpp
+++ src/gameobject.cpp
@@ -1,5 +1,12 @@
 #include "gameobject.h"
 
+// -------------------------------------------------------------
+// Function: ~GameObject()
+// Description: Virtual class destructor method
+// Parameters: void
+// Atributes: nothing
+// Return: void
+// -------------------------------------------------------------
 GameObject::~GameObject() {
     int size = children.size();
     for(int i = 0; i < size; i++) {
@@ -9,10 +16,26 @@ GameObject::~GameObject() {
     }
 }
 
+// -------------------------------------------------------------
+// Function: addChild()
+// Description: Adds a element to the vector GamesObject*
+// Parameters:
+//		GameObject* child;		Pointer to the game object in question
+// Atributes: nothing
+// Return: void
+// -------------------------------------------------------------
 void GameObject::addChild(GameObject* child) {
     children.push_back(child);
 }
 
+// -------------------------------------------------------------
+// Function: draw()
+// Description: Method that draws a object on the screen
+// Parameters:
+//		SDL_Surface *surface;		Pointer for the surface where the graphic component will be drawn.
+// Atributes: nothing
+// Return: void
+// -------------------------------------------------------------
 void GameObject::draw(SDL_Surface *surface) {
     drawSelf(surface);
     int size = children.size();
