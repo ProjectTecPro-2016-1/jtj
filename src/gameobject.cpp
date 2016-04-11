2bf8b2454438df0497eaa65ecbe4548927856b88
Cafeina <climaco.cafe@gmail.com>
2016-04-11 13:13:20 -0300

    Removing comments from cpp file of GameObject class

--- src/gameobject.cpp
+++ src/gameobject.cpp
@@ -1,12 +1,5 @@
 #include "gameobject.h"
 
-// -------------------------------------------------------------
-// Function: ~GameObject()
-// Description: Virtual class destructor method
-// Parameters: void
-// Atributes: nothing
-// Return: void
-// -------------------------------------------------------------
 GameObject::~GameObject() {
     int size = children.size();
     for(int i = 0; i < size; i++) {
@@ -16,26 +9,10 @@ GameObject::~GameObject() {
     }
 }
 
-// -------------------------------------------------------------
-// Function: addChild()
-// Description: Adds a element to the vector GamesObject*
-// Parameters:
-//		GameObject* child;		Pointer to the game object in question
-// Atributes: nothing
-// Return: void
-// -------------------------------------------------------------
 void GameObject::addChild(GameObject* child) {
     children.push_back(child);
 }
 
-// -------------------------------------------------------------
-// Function: draw()
-// Description: Method that draws a object on the screen
-// Parameters:
-//		SDL_Surface *surface;		Pointer for the surface where the graphic component will be drawn.
-// Atributes: nothing
-// Return: void
-// -------------------------------------------------------------
 void GameObject::draw(SDL_Surface *surface) {
     drawSelf(surface);
     int size = children.size();
