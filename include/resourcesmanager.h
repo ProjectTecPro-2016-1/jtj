61bb4cb943d30056b82d61c97488a0339529c08c
Cafeina <climaco.cafe@gmail.com>
2016-04-14 09:11:27 -0300

    Adding description to ResourceManager attributes and methods in the header file

--- include/resourcesmanager.h
+++ include/resourcesmanager.h
@@ -5,10 +5,19 @@
 #include <vector>
 
 class ResourcesManager {
-    ResourcesManager();
-    static ResourcesManager* instance = NULL;
 
-    class ResourcesInfo {
+    // -------------------------------------------------------------
+	// Function: ResourcesManager()
+	// Description: ResourcesManager class builder.
+	// Parameters: void
+	// Attributes: nothing
+	// Return: void
+	// Observation: Without implementation.
+	// -------------------------------------------------------------
+    ResourcesManager();
+    static ResourcesManager* instance = NULL;   // Pointer to instance resources.
+
+    class ResourcesInfo { // Contain information of resources.
         Resource* resource;
         int references;
         public:
@@ -18,12 +27,42 @@ class ResourcesManager {
             }
     };
 
-    vector<ResourcesInfo*> resources;
+    vector<ResourcesInfo*> resources;   // Vector that contains information of resources.
 
     public:
         static ResourcesManager *getInstance();
+
+        // -------------------------------------------------------------
+    	// Function: getImage()
+    	// Description:
+    	// Parameters:
+    	// 		const string &name;
+    	// Attributes: nothing
+    	// Return: RawImage *
+    	// Observation: Without implementation.
+    	// -------------------------------------------------------------
         RawImage* getImage(const string &name);
+
+        // -------------------------------------------------------------
+    	// Function: release()
+    	// Description:
+    	// Parameters:
+    	// 		RawImage *image;
+    	// Attributes: nothing
+    	// Return: void
+    	// Observation: Without implementation.
+    	// -------------------------------------------------------------
         void release(RawImage* image);
+
+        // -------------------------------------------------------------
+    	// Function: load()
+    	// Description:
+    	// Parameters:
+    	// 		const string &resourcesDescription;
+    	// Attributes: nothing
+    	// Return: void
+    	// Observation: Without implementation.
+    	// -------------------------------------------------------------
         void load(const string &resourcesDescription);
 };
 #endif
