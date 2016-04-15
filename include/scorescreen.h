8d7917ddbe563be0926a236278bb748d35592745
Carolina Ferreira <carol.mf14@gmail.com>
2016-04-15 18:08:44 -0300

    comments about atributtes and contants were add to '.h' of ScoreScreen

--- include/scorescreen.h
+++ include/scorescreen.h
@@ -6,37 +6,35 @@
 #include "SDL/SDL_ttf.h"
 #include <string>
 
-class ScoreScreen
-{
-public:
-    ScoreScreen();
-    ~ScoreScreen();
-    void drawSelf(SDL_Surface *surface);
-    int getLine();
-    int getScorePoints();
-    void popLine();
-    void increaseScore(int value);
-    void lines(int numero);
-    void scoring(int value);
+class ScoreScreen {
+	public:
+		
+		ScoreScreen();
+		~ScoreScreen();
+		void drawSelf(SDL_Surface *surface);
+		int getLine();
+		int getScorePoints();
+		void popLine();
+		void increaseScore(int value);
+		void lines(int numero);
+		void scoring(int value);
 
-private:
+	private:
 
-    void updateSelf();
+		void updateSelf();
+		SDL_Surface *armario;
+		TTF_Font *scoreFont;
+		SDL_Surface *scoreMessage;
+		SDL_Surface *boxMessage;
+		SDL_Color scoreTextColor;
+		char scoreString[30];
+		char lineString[30];
+		int scorePoints;
+		int lineLeft;
 
-    SDL_Surface *armario;
-    TTF_Font *scoreFont;
-    SDL_Surface *scoreMessage;
-    SDL_Surface *boxMessage;
-    SDL_Color scoreTextColor;
-    char scoreString[30];
-    char lineString[30];
-    int scorePoints;
-    int lineLeft;
-
-    static const int SCORE_WIDTH = 282;
-    static const int SCORE_HEIGHT = 456;
-    static const int SCORE_X_OFFSET = 506;
-    static const int SCORE_Y_OFFSET = 10;
+		static const int SCORE_WIDTH = 282;
+		static const int SCORE_HEIGHT = 456;
+		static const int SCORE_X_OFFSET = 506;
+		static const int SCORE_Y_OFFSET = 10;
 };
-
 #endif
