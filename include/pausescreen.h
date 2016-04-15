3884e648645a4c4451f0d202d0e8ebceac96bbc6
Carolina Ferreira <carol.mf14@gmail.com>
2016-04-15 18:07:49 -0300

    comments about atributtes and constants were added to '.h' of PauseScreen

--- include/pausescreen.h
+++ include/pausescreen.h
@@ -16,5 +16,4 @@ class PauseScreen : public GameObject {
         static const int LEVEL_WIDTH = 854;			// Size of width screen that will show the image level, in pixels.
         static const int LEVEL_HEIGHT = 480;		// Size of height screen that will show the image level, in pixels.
 };
-
 #endif
