b947368f28e86e82fd7262e6afdd3102f9b069ca
Carolina Ferreira <carol.mf14@gmail.com>
2016-04-15 18:03:48 -0300

    comments about atributtes and constants were add to the '.h' of GameOverScreen

--- include/gameoverscreen.h
+++ include/gameoverscreen.h
@@ -5,7 +5,6 @@
 #include <string>
 
 class GameOverScreen : public GameObject {
-	
     SDL_Surface * gameOverScreen;		// Keeps the image that loaded.
 	void drawSelf(SDL_Surface * surface);
 	
@@ -17,5 +16,4 @@ class GameOverScreen : public GameObject {
         static const int LEVEL_WIDTH = 854;			// Size of width screen that will show the image level, in pixels.
         static const int LEVEL_HEIGHT = 480;		// Size of height screen that will show the image level, in pixels.			
 };
-
 #endif
