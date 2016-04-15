7c8a6fcb59840ae01fc8d134db22c1e87664a9c1
Carolina Ferreira <carol.mf14@gmail.com>
2016-04-15 18:06:25 -0300

    comments about atributtes and constants were added to '.h' of InitScreen

--- include/initscreen.h
+++ include/initscreen.h
@@ -17,5 +17,4 @@ class InitScreen : public GameObject {
         static const int LEVEL_WIDTH = 854;			// Size of width screen that will show the image level, in pixels.
         static const int LEVEL_HEIGHT = 480;		// Size of height screen that will show the image level, in pixels.
 };
-
 #endif
