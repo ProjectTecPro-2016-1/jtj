e792113a28236101b4e12d9983d3de11d8d1ee31
Cafeina <climaco.cafe@gmail.com>
2016-04-16 13:06:05 -0300

    Removing from header file description of Game Object class methods in order to put them in cpp file

--- include/gameobject.h
+++ include/gameobject.h
@@ -9,40 +9,10 @@ using namespace std;
 class GameObject {
 
     vector<GameObject*> children;   // Vector that stocks the objects of the game
-
-    // -------------------------------------------------------------
-	// Function: drawSelf()
-	// Description: Draws the game object in a specific screen position according to the box attributes.
-	// Parameters:
-	//		SDL_Surface *surface;		Pointer for the surface where the graphic component will be drawn.
-	// Return: void
-	// -------------------------------------------------------------
     virtual void drawSelf(SDL_Surface * surface) = 0;
     public:
-
-        // -------------------------------------------------------------
-    	// Function: ~GameObject()
-    	// Description: Virtual class destructor method
-    	// Return: void
-    	// -------------------------------------------------------------
         virtual ~GameObject();
-
-        // -------------------------------------------------------------
-    	// Function: draw()
-    	// Description: Method that draws a object on the screen
-    	// Parameters:
-    	//		SDL_Surface *surface;		Pointer for the surface where the graphic component will be drawn.
-    	// Return: void
-    	// -------------------------------------------------------------
         void draw(SDL_Surface * surface);
-
-        // -------------------------------------------------------------
-    	// Function: addChild()
-    	// Description: Adds a element to the vector GamesObject*
-    	// Parameters:
-    	//		GameObject* child;		Pointer to the game object in question
-    	// Return: void
-    	// -------------------------------------------------------------
         void addChild(GameObject * child);
 };
 
