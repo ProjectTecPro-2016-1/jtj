227fc06eea923cb6004cbb2b3f7e29c21fb0c8e4
Cafeina <climaco.cafe@gmail.com>
2016-04-16 14:23:53 -0300

    Adding description of Box class methods to cpp file

--- src/box.cpp
+++ src/box.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 
+// -------------------------------------------------------------
+// Function: drawSelf()
+// Description: Draws the box in a specific screen position according to the box attributes.
+// Parameters:
+//		SDL_Surface * surface;		Pointer for the surface where the graphic component will be
+//                                  drawn.
+// Return: void
+// -------------------------------------------------------------
 void Box::drawSelf(SDL_Surface * surface) {
     if (used == true) {
         SDLUtil::applySurface(this->x_position, this->y_position, this->box, surface);
@@ -12,6 +20,13 @@ void Box::drawSelf(SDL_Surface * surface) {
     }
 }
 
+// -------------------------------------------------------------
+// Function: Box()
+// Description: Box class builder where the inicialization of the class variables happen.
+// Parameters:
+//		string fileName;  		Box landscape file name.
+// Return: void
+// -------------------------------------------------------------
 Box::Box(string filename) {
     this->box = SDLUtil::loadImage(filename);
     x_position = 0;
@@ -21,35 +36,77 @@ Box::Box(string filename) {
     used = false;
 }
 
+// -------------------------------------------------------------
+// Function: ~Box()
+// Description: Box class destructor where the box image files free themselves.
+// Return: void
+// -------------------------------------------------------------
 Box::~Box() {
     if (box != NULL) {
         SDL_FreeSurface(box);
     } else {
         // Nothing to do
     }
-        
+
 }
 
+// -------------------------------------------------------------
+// Function: getPositionX()
+// Description: Returns a position in the box X axis at the right moment.
+// Return: int
+// -------------------------------------------------------------
 int Box::getPositionX() {
     return this->x_position;
 }
 
+// -------------------------------------------------------------
+// Function: getPositionY()
+// Description: Returns a position in the box Y axis at the right moment.
+// Return: int
+// -------------------------------------------------------------
 int Box::getPositionY() {
     return this->y_position;
 }
 
+// -------------------------------------------------------------
+// Function: getSpeed()
+// Description: Returns the speed which the box is moving at the right moment.
+// Return: int
+// -------------------------------------------------------------
 int Box::getSpeed() {
     return this->speed;
 }
 
+// -------------------------------------------------------------
+// Function: setPosition()
+// Description: Return the speed which the box is moving at the right moment.
+// Parameters:
+//		int x;			Insert the value of axis X of the box on the attribute that keeps the information
+//		int y;  		Insert the value of axis Y of the box on the attribute that keeps the information
+// Return: void
+// -------------------------------------------------------------
 void Box::setPosition(int x, int y) {
     this->x_position = x;
     this->y_position = y;
 }
 
+// -------------------------------------------------------------
+// Function: accelerate()
+// Description: Increase the speed of the box at the right moment.
+// Return: void
+// Observations:
+// 		- Funtion not implemented
+// -------------------------------------------------------------
 void Box::accelerate() {
 }
 
+// -------------------------------------------------------------
+// Function: fall()
+// Description: Control the box fall during the execution of the level of the game.
+// Parameters:
+//		int vector<Box*> grid[12];			Vector that contain the box positions in the platform.
+// Return: void
+// -------------------------------------------------------------  
 void Box::fall(vector<Box*>grid[12]) {
     if (lyingDown == false) {
         speed += ACCELERATION;
