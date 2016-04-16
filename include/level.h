914c236a493f55cd15d0c561ebfc0d91b5e11bb2
Cecilia <ceciliabessa@hotmail.com>
2016-04-16 15:15:59 -0300

    Inserting descriptions in Level class

--- include/level.h
+++ include/level.h
@@ -8,24 +8,23 @@
 #include <vector>
 
 class Level : public GameObject {
-    SDL_Surface * level;
-    void drawSelf(SDL_Surface *surface);
+    SDL_Surface * level;        // Pointer for level graphic elements.
+    void drawSelf(SDL_Surface * surface);
     //this stack shall handle the colision logic.
     public:
-        vector<Enemy*> enemies;
-		vector<Box*> boxes;
-		vector<Box*> grid[12];
+        vector<Enemy*> enemies;     // Vector that contains info about enemies.
+		vector<Box*> boxes;         // Vector that contains info about boxes.
+		vector<Box*> grid[12];      // Vector that contains the platform where the box falls.
 
-        static const int LEVEL_WIDTH = 456;
-        static const int LEVEL_HEIGHT = 456;
-        static const int LEVEL_X_OFFSET = 50;
-        static const int LEVEL_Y_OFFSET = 10;
+        static const int LEVEL_WIDTH = 456;         // All level pixel width.
+        static const int LEVEL_HEIGHT = 456;        // All level pixel height.
+        static const int LEVEL_X_OFFSET = 50;       // Where the level is drawn on the specified surface in axis X.
+        static const int LEVEL_Y_OFFSET = 10;       // Where the level is drawn on the specified surface in axis Y.
 
         Level(std::string filename);
         ~Level();
 
 		vector<Box*> getBoxes();
-
 };
 
 #endif
--- src/level.cpp
+++ src/level.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+// -------------------------------------------------------------  
+// Function: Level()  
+// Description: Level class builder where the inicialization of the class variables happen.
+// Parameters:
+//		string filename;		Level landscape file name.
+// Return: void  
+// -------------------------------------------------------------  
 Level::Level(string filename) {
     this->level = SDLUtil::loadImage(filename);
     //for(unsigned int i = 0; i < sizeof(grid)/sizeof(int); i++) {
@@ -10,6 +17,11 @@ Level::Level(string filename) {
     //}
 }
 
+// -------------------------------------------------------------  
+// Function: ~Level()  
+// Description: Level class destructor where the level image files free themselves.
+// Return: void  
+// -------------------------------------------------------------  
 Level::~Level() {
     if (level != NULL) {
         SDL_FreeSurface(level);
@@ -18,11 +30,23 @@ Level::~Level() {
     }
 }
 
-//draws the level on the specified surface
+// -------------------------------------------------------------  
+// Function: drawSelf()  
+// Description: Draws the level on the specified surface.
+// Parameters:
+//		SDL_Surface *surface;		Pointer for the surface where the graphic component will be drawn.
+// Return: void  
+// -------------------------------------------------------------
 void Level::drawSelf(SDL_Surface * surface) {
     SDLUtil::applySurface(Level::LEVEL_X_OFFSET, Level::LEVEL_Y_OFFSET, this->level, surface);
     return;
 }
+
+// -------------------------------------------------------------  
+// Function: getBoxes()  
+// Description: Returns a vector whith all box positions.
+// Return: vector<Box*>  
+// -------------------------------------------------------------
 vector<Box*> Level::getBoxes() {
 	return boxes;
 }
