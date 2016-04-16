f5e29a00864abf64102bbfb85fbcf3fec5412544
Cafeina <climaco.cafe@gmail.com>
2016-04-16 19:13:05 -0300

    Adjusting SDLUtil class according to stylesheet

--- include/sdlutil.h
+++ include/sdlutil.h
@@ -7,7 +7,8 @@
 class SDLUtil {
     public:
         static SDL_Surface * loadImage(std::string filename);
-        static void applySurface(int x, int y, SDL_Surface * source, SDL_Surface * destination, SDL_Rect * clip = NULL);
+        static void applySurface(int x, int y, SDL_Surface * source, SDL_Surface * destination,
+                                SDL_Rect * clip = NULL);
 };
 
 #endif
