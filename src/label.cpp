6827782db80b3a6bf2de72323ef11f46c856f545
Dib <ceciliabessa@hotmail.com>
2016-05-06 10:35:34 -0300

    Label class: Comments, code self-explanatory and API

--- src/label.cpp
+++ src/label.cpp
@@ -3,50 +3,50 @@
 #include "sdlutil.h"
 #include "level.h"
 
-// -------------------------------------------------------------  
-// Function: Label()  
+// -------------------------------------------------------------
+// Function: Label()
 // Description: Label class builder where the inicialization of the class variables happen.
 // Parameters:
 //		string filename;		Label landscape file name.
 //		int xPosition;			Where the label is localizated in axis X.
 //		int yPosition;			Where the label is localizated in axis Y.
-// Return: void  
+// Return: void
 // -------------------------------------------------------------
 Label::Label(std::string filename, int xPosition, int yPosition) {
-	assert(filename != "" && "Impossible to loadImage, because the image filename of label is empty");
-	assert(xPosition > 0 && "xPosition needs to be a positive value");
-	assert(yPosition > 0 && "yPosition needs to be a positive value");	
-	this->label = SDLUtil::loadImage(filename);
-	this->xPosition = xPosition;
-	this->yPosition = yPosition;
+    assert(filename != "" && "Impossible to loadImage, because the image filename of label is empty");
+    assert(xPosition > 0 && "xPosition needs to be a positive value");
+    assert(yPosition > 0 && "yPosition needs to be a positive value");
+    this->label = SDLUtil::loadImage(filename);
+    this->xPosition = xPosition;
+    this->yPosition = yPosition;
 }
 
-// -------------------------------------------------------------  
-// Function: ~Label()  
+// -------------------------------------------------------------
+// Function: ~Label()
 // Description: Label class destructor where the level image files free themselves.
-// Return: void  
+// Return: void
 // -------------------------------------------------------------
 Label::~Label() {
-	if (label != NULL) {
-		SDL_FreeSurface(label);
-	} else {
-        // Nothing to do
-    }
+    if (label != NULL) {
+           SDL_FreeSurface(label);
+    } else {
+            // Nothing to do
+     }
 }
 
-// -------------------------------------------------------------  
-// Function: drawSelf()  
+// -------------------------------------------------------------
+// Function: drawSelf()
 // Description: Draws the label on the specified surface.
 // Parameters:
 //		SDL_Surface * surface;		Pointer for the surface where the graphic component will be drawn.
-// Return: void  
+// Return: void
 // -------------------------------------------------------------
 void Label::drawSelf(SDL_Surface * surface) {
-	SDLUtil::applySurface(this->xPosition, this->yPosition, this->label, surface);
+    SDLUtil::applySurface(this->xPosition, this->yPosition, this->label, surface);
 }
 
-// -------------------------------------------------------------  
-// Function: wasClicked()  
+// -------------------------------------------------------------
+// Function: wasClicked()
 // Description: Where was clicked by the user
 // Parameters:
 //		int xMouse;		Where the mouse is localizated in axis X.
@@ -54,11 +54,10 @@ void Label::drawSelf(SDL_Surface * surface) {
 // Return: bool
 // -------------------------------------------------------------
 bool Label::wasClicked(int xMouse, int yMouse) {
-	if (xMouse > this->xPosition && xMouse < this->xPosition + LABEL_WIDTH && yMouse > this->yPosition && yMouse < this->yPosition + LABEL_HEIGHT) {
-		return true;
-	} else {
-        // Nothing to do
+    if (xMouse > this->xPosition && xMouse < this->xPosition + LABEL_WIDTH && yMouse > this->yPosition
+            && yMouse < this->yPosition + LABEL_HEIGHT) {
+           return true;
+    } else {
+           return false;
     }
-
-	return false;
 }
