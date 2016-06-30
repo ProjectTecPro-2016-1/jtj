df595c9e971e9977215dd057b48b4437af47cf7c
Dib <ceciliabessa@hotmail.com>
2016-06-29 21:58:23 -0300

    Refactoring jack class

--- src/jack.cpp
+++ src/jack.cpp
@@ -13,18 +13,16 @@ using namespace std;
 // -------------------------------------------------------------
 Jack::Jack(string filename) {
     this->jack = SDLUtil::loadImage(filename);
-    //this->x_position = JACK_WIDTH + Level::LEVEL_X_OFFSET;
     setXPosition(JACK_WIDTH + Level::LEVEL_X_OFFSET);
-    //this->y_position = Level::LEVEL_HEIGHT + Level::LEVEL_Y_OFFSET - Jack::JACK_HEIGHT - 38;
     setYPosition(Level::LEVEL_HEIGHT + Level::LEVEL_Y_OFFSET - Jack::JACK_HEIGHT - 38);
-    this->speed = 0;
-    this->dead = false;
-    verticalSpeed = 0;
-    jumping = false;
-	lastMove = 0;
-	lastButOneMove = 0;
-	strength = 0;
-    frame = 0;
+    setSpeed(0);
+    setDead(false);
+    setVerticalSpeed(0);
+    setJumping(false);
+    setLastMove(0);
+    setLastButOneMove(0);
+    setStrength(0);
+    setFrame(0);
 }
 
 // -------------------------------------------------------------
@@ -46,7 +44,7 @@ Jack::~Jack() {
 // Return: void
 // -------------------------------------------------------------
 void Jack::die() {
-    this->dead = true;
+    setDead(true);
 }
 
 // -------------------------------------------------------------
@@ -55,7 +53,7 @@ void Jack::die() {
 // Return: bool
 // -------------------------------------------------------------
 bool Jack::isDead() {
-    return this->dead;
+    return getDead();
 }
 
 // -------------------------------------------------------------
@@ -93,40 +91,39 @@ int Jack::setLimit(int value, int limit1, int range) {
 // Return: void
 // -------------------------------------------------------------
 void Jack::drawSelf(SDL_Surface *surface) {
-    if (jumping == true) {
-        if(speed >= 0) {
-            frame = 7;
+    if (getJumping() == true) {
+        if(getSpeed() >= 0) {
+            setFrame(7);
         } else {
-            frame = 8;
+            setFrame(8);
         }
-    } else if (speed > 0) {
-        frame ++;
+    } else if (getSpeed() > 0) {
+        setFrame(getFrame() + 1);
 
-        if (frame > 3) {
-            frame = 1;
-        } else {
-            // Nothing to do
-        }
-    } else if (speed < 0) {
-        frame++;
-
-        if (frame < 4) {
-            frame = 4;
+        if (getFrame() > 3) {
+            setFrame(1);
         } else {
             // Nothing to do
         }
+    } else if (getSpeed() < 0) {
+        setFrame(getFrame() +1);
 
-        if (frame > 6) {
-            frame = 4;
+        if (getFrame() < 4) {
+            setFrame(4);
+        } else {
+            // Nothing to do
+        }
+
+        if (getFrame() > 6) {
+            setFrame(4);
         } else {
             // Nothing to do
         }
     } else {
-        frame = 0;
+        setFrame(0);
     }
 
-    SDLUtil::applySurface(this->x_position, this->y_position, this->jack, surface, &spriteClips[frame]);
-    //SDLUtil::applySurface(getXPosition(), getYPosition(), this->jack, surface, &spriteClips[frame]);
+    SDLUtil::applySurface(getXPosition(), getYPosition(), this->jack, surface, &spriteClips[getFrame()]);
     return;
 }
 
@@ -140,13 +137,10 @@ void Jack::drawSelf(SDL_Surface *surface) {
 //		int yRange;		Range in Y axis.
 // Return: void
 // -------------------------------------------------------------
-void Jack::move(int xBegin, int xRange, int yBegin, int yRange) {
-    // x_position += speed;
-    setXPosition(getXPosition() + speed);
+ void Jack::move(int xBegin, int xRange, int yBegin, int yRange) {
+    setXPosition(getXPosition() + getSpeed());
 
-    //x_position = setLimit(x_position, xBegin, xRange - JACK_WIDTH);
     setXPosition(setLimit(getXPosition(), xBegin, xRange - JACK_WIDTH));
-    //this->y_position = setLimit(y_position, yBegin, yRange - JACK_HEIGHT - 38);
     setYPosition(setLimit(getYPosition(), yBegin, yRange - JACK_HEIGHT - 38));
 
     return;
@@ -162,24 +156,20 @@ void Jack::move(int xBegin, int xRange, int yBegin, int yRange) {
 // -------------------------------------------------------------
 void Jack::jump(Level* level) {
 
-    //y_position += verticalSpeed;
-    setYPosition(getYPosition() + verticalSpeed);
-    verticalSpeed += ACCELERATION;
+    setYPosition(getYPosition() + getVerticalSpeed());
+    setVerticalSpeed(getVerticalSpeed() + ACCELERATION);
 
     int maxJumpHeightLeft = 0;
     maxJumpHeightLeft = (int)(Level::LEVEL_Y_OFFSET + Level::LEVEL_HEIGHT - 57 - 38 -
-                                (level->grid[(getXPosition() - Level::LEVEL_X_OFFSET) / 38].size() * 38));
-    //                         (level->grid[(x_position - Level::LEVEL_X_OFFSET) / 38].size() * 38));
+                                          (level->grid[(getXPosition() - Level::LEVEL_X_OFFSET) / 38].size() * 38));
     int maxJumpHeightRight = 0;
     maxJumpHeightRight = (int)(Level::LEVEL_Y_OFFSET + Level::LEVEL_HEIGHT - 57 - 38 -
-                              (level->grid[(getXPosition() + 37 - Level::LEVEL_X_OFFSET) / 38].size() * 38));
-    //                        (level->grid[(x_position + 37 - Level::LEVEL_X_OFFSET) / 38].size() * 38));
+                                          (level->grid[(getXPosition() + 37 - Level::LEVEL_X_OFFSET) / 38].size() * 38));
 
-    if (jumping == true) {
-        //if ((y_position >= maxJumpHeightLeft) || (y_position >= maxJumpHeightRight)) {
+    if (getJumping() == true) {
         if ((getYPosition() >= maxJumpHeightLeft) || (getYPosition() >= maxJumpHeightRight)) {
-            jumping = false;
-            verticalSpeed = 1;
+            setJumping(false);
+            setVerticalSpeed(1);
         } else {
             // Nothing to do
         }
@@ -187,26 +177,29 @@ void Jack::jump(Level* level) {
         // Nothing to do
     }
 
-    //these next two if's handles boxes superior colision.
-    //if (y_position >= maxJumpHeightLeft) {
-    if (getYPosition() >= maxJumpHeightLeft) {
-        //y_position = maxJumpHeightLeft;
-        setYPosition(maxJumpHeightLeft);
-
-        verticalSpeed = 1;
-        //verticalSpeed -= ACCELERATION;
-    //} else if (y_position >= maxJumpHeightRight) {
-    } else if (getYPosition() >= maxJumpHeightRight) {
-        //y_position = maxJumpHeightRight;
-        setYPosition(maxJumpHeightRight);
-        verticalSpeed = 1;
-        //verticalSpeed -= ACCELERATION;
-    } else { //this avoids double jumping while falling from a box
-        jumping = true;
-    }
-    //verticalSpeed++;
+    controlsMaxJump(maxJumpHeightLeft, maxJumpHeightRight);
 
     return;
+}
+
+void Jack::controlsMaxJump(int maxJumpHeightLeft, int maxJumpHeightRight) {
+    //these next two if's handles boxes superior colision.
+    if (getYPosition() >= maxJumpHeightLeft) {
+        setYPosition(maxJumpHeightLeft);
+
+        setVerticalSpeed(1);
+        //verticalSpeed -= ACCELERATION;
+
+    } else if (getYPosition() >= maxJumpHeightRight) {
+        setYPosition(maxJumpHeightRight);
+
+        setVerticalSpeed(1);
+        //verticalSpeed -= ACCELERATION;
+
+    } else { //this avoids double jumping while falling from a box
+        setJumping(true);
+    }
+    //verticalSpeed++;
 }
 
 // -------------------------------------------------------------
@@ -217,14 +210,14 @@ void Jack::jump(Level* level) {
 // Return: void
 // -------------------------------------------------------------
 void Jack::pushMove(int v) {
-	if (lastMove == 0) {
-		speed = v;
-		lastMove = v;
-	} else if (lastButOneMove == 0) {
-		speed = v;
-		lastButOneMove = v;
+	if (getLastMove() == 0) {
+		setSpeed(v);
+		setLastMove(v);
+	} else if (getLastButOneMove() == 0) {
+		setSpeed(v);
+		setLastButOneMove(v);
 	} else {
-        // Nothing to do
+                    // Nothing to do
     }
 
     return;
@@ -238,18 +231,18 @@ void Jack::pushMove(int v) {
 // Return: void
 // -------------------------------------------------------------
 void Jack::popMove(int v) {
-	if (lastButOneMove == v) {
-		lastButOneMove = 0;
-		speed = lastMove;
-	} else if (lastMove == v) {
-		if (lastButOneMove != 0) {
-			speed = lastButOneMove;
-			lastMove = lastButOneMove;
-			lastButOneMove = 0;
+	if (getLastButOneMove() == v) {
+		setLastButOneMove(0);
+		setSpeed(getLastMove());
+	} else if (getLastMove() == v) {
+		if (getLastButOneMove() != 0) {
+			setSpeed(getLastButOneMove());
+			setLastMove(getLastButOneMove());
+			setLastButOneMove(0);
 		}
 		else {
-			lastMove = 0;
-			speed = 0;
+			setLastMove(0);
+			setSpeed(0);
 			/*
 			int rest = x_position%Box::BOX_WIDTH;
 			if(rest<10) {
@@ -261,10 +254,9 @@ void Jack::popMove(int v) {
 			*/
 		}
 	} else {
-        // Nothing to do
+                    // Nothing to do
     }
 }
-
 
 // -------------------------------------------------------------
 // Function: pushBox()
@@ -276,56 +268,84 @@ void Jack::popMove(int v) {
 void Jack::pushBox() {
 }
 
+void Jack::setSpriteClipsStopped() {
+    spriteClips[0].x = 0;
+    spriteClips[0].y = JACK_HEIGHT;
+    spriteClips[0].w = JACK_WIDTH;
+    spriteClips[0].h = JACK_HEIGHT;
+}
+
+void Jack::setSpriteClipsOneRight() {
+    spriteClips[1].x = JACK_WIDTH;
+    spriteClips[1].y = JACK_HEIGHT;
+    spriteClips[1].w = JACK_WIDTH;
+    spriteClips[1].h = JACK_HEIGHT;
+}
+
+void Jack::setSpriteClipsSecondRight() {
+    spriteClips[2].x = JACK_WIDTH*2;
+    spriteClips[2].y = JACK_HEIGHT;
+    spriteClips[2].w = JACK_WIDTH;
+    spriteClips[2].h = JACK_HEIGHT;
+}
+
+void Jack::setSpriteClipsThirdRight() {
+    spriteClips[3].x = JACK_WIDTH*3;
+    spriteClips[3].y = JACK_HEIGHT;
+    spriteClips[3].w = JACK_WIDTH;
+    spriteClips[3].h = JACK_HEIGHT;
+}
+
+void Jack::setSpriteClipsJumpRight() {
+    spriteClips[7].x = JACK_WIDTH*4;
+    spriteClips[7].y = JACK_HEIGHT;
+    spriteClips[7].w = JACK_WIDTH;
+    spriteClips[7].h = JACK_HEIGHT;
+}
+
+void Jack::setSpriteClipsOneLeft() {
+    spriteClips[4].x = JACK_WIDTH;
+    spriteClips[4].y = 0;
+    spriteClips[4].w = JACK_WIDTH;
+    spriteClips[4].h = JACK_HEIGHT;
+}
+
+void Jack::setSpriteClipsSecondLeft() {
+    spriteClips[5].x = JACK_WIDTH*2;
+    spriteClips[5].y = 0;
+    spriteClips[5].w = JACK_WIDTH;
+    spriteClips[5].h = JACK_HEIGHT;
+}
+
+void Jack::setSpriteClipsThirdLeft() {
+    spriteClips[6].x = JACK_WIDTH*3;
+    spriteClips[6].y = 0;
+    spriteClips[6].w = JACK_WIDTH;
+    spriteClips[6].h = JACK_HEIGHT;
+}
+
+void Jack::setSpriteClipsJumpLeft() {
+    spriteClips[8].x = JACK_WIDTH*4;
+    spriteClips[8].y = 0;
+    spriteClips[8].w = JACK_WIDTH;
+    spriteClips[8].h = JACK_HEIGHT;
+}
+
 // -------------------------------------------------------------
 // Function: setSpriteClips()
 // Description: Initializes the dimensions of each picture frame and each Jack's moviment.
 // Return: void
 // -------------------------------------------------------------
 void Jack::setSpriteClips() {
-    spriteClips[0].x = 0;
-    spriteClips[0].y = JACK_HEIGHT;
-    spriteClips[0].w = JACK_WIDTH;
-    spriteClips[0].h = JACK_HEIGHT;
-
-    spriteClips[1].x = JACK_WIDTH;
-    spriteClips[1].y = JACK_HEIGHT;
-    spriteClips[1].w = JACK_WIDTH;
-    spriteClips[1].h = JACK_HEIGHT;
-
-    spriteClips[2].x = JACK_WIDTH*2;
-    spriteClips[2].y = JACK_HEIGHT;
-    spriteClips[2].w = JACK_WIDTH;
-    spriteClips[2].h = JACK_HEIGHT;
-
-    spriteClips[3].x = JACK_WIDTH*3;
-    spriteClips[3].y = JACK_HEIGHT;
-    spriteClips[3].w = JACK_WIDTH;
-    spriteClips[3].h = JACK_HEIGHT;
-
-    spriteClips[4].x = JACK_WIDTH;
-    spriteClips[4].y = 0;
-    spriteClips[4].w = JACK_WIDTH;
-    spriteClips[4].h = JACK_HEIGHT;
-
-    spriteClips[5].x = JACK_WIDTH*2;
-    spriteClips[5].y = 0;
-    spriteClips[5].w = JACK_WIDTH;
-    spriteClips[5].h = JACK_HEIGHT;
-
-    spriteClips[6].x = JACK_WIDTH*3;
-    spriteClips[6].y = 0;
-    spriteClips[6].w = JACK_WIDTH;
-    spriteClips[6].h = JACK_HEIGHT;
-
-    spriteClips[7].x = JACK_WIDTH*4;
-    spriteClips[7].y = JACK_HEIGHT;
-    spriteClips[7].w = JACK_WIDTH;
-    spriteClips[7].h = JACK_HEIGHT;
-
-    spriteClips[8].x = JACK_WIDTH*4;
-    spriteClips[8].y = 0;
-    spriteClips[8].w = JACK_WIDTH;
-    spriteClips[8].h = JACK_HEIGHT;
+    setSpriteClipsStopped();
+    setSpriteClipsOneRight();
+    setSpriteClipsSecondRight();
+    setSpriteClipsThirdRight();
+    setSpriteClipsJumpRight();
+    setSpriteClipsOneLeft();
+    setSpriteClipsSecondLeft();
+    setSpriteClipsThirdLeft();
+    setSpriteClipsJumpLeft();
 }
 
 // -------------------------------------------------------------
@@ -337,6 +357,14 @@ int Jack::getXPosition() {
     return this->x_position;
 }
 
+// -------------------------------------------------------------
+// Function: setXPosition()
+// Description: Return the Jack's position on the X axis.
+// Parameters:
+//      int x_position;          Insert the value of axis X of jack on the attribute that keeps the
+//                                        information
+// Return: void
+// -------------------------------------------------------------
 void Jack::setXPosition(int x_position) {
     this->x_position =  x_position;
 }
@@ -350,6 +378,89 @@ int Jack::getYPosition() {
     return this->y_position;
 }
 
+// -------------------------------------------------------------
+// Function: setYPosition()
+// Description: Return the Jack's position on the Y axis.
+// Parameters:
+//      int y_position;          Insert the value of axis Y of jack on the attribute that keeps the
+//                                        information
+// Return: void
+// -------------------------------------------------------------
 void Jack::setYPosition(int y_position) {
     this->y_position =  y_position;
 }
+
+// -------------------------------------------------------------
+// Function: getSpeed()
+// Description: Return the Jack's speed.
+// Return: int
+// -------------------------------------------------------------
+int Jack::getSpeed() {
+    return this->speed;
+}
+
+// -------------------------------------------------------------
+// Function: setSpeed()
+// Description: Return the Jack's speed.
+// Return: void
+// -------------------------------------------------------------
+void Jack::setSpeed(int speed) {
+    this->speed =  speed;
+}
+
+bool Jack::getDead() {
+    return this->dead;
+}
+
+void Jack::setDead(bool dead) {
+    this->dead = dead;
+}
+
+int Jack::getVerticalSpeed() {
+    return this->verticalSpeed;
+}
+
+void Jack::setVerticalSpeed(int verticalSpeed) {
+    this->verticalSpeed = verticalSpeed;
+}
+
+bool Jack::getJumping() {
+    return this->jumping;
+}
+
+void Jack::setJumping(bool jumping) {
+    this->jumping = jumping;
+}
+
+int Jack::getLastMove() {
+    return this->lastMove;
+}
+
+void Jack::setLastMove(int lastMove) {
+    this->lastMove = lastMove;
+}
+
+int Jack::getLastButOneMove() {
+    return this->lastButOneMove;
+}
+
+void Jack::setLastButOneMove(int lastButOneMove) {
+    this->lastButOneMove = lastButOneMove;
+}
+
+int Jack::getStrength() {
+    return this->strength;
+}
+
+void Jack::setStrength(int strength) {
+    this->strength = strength;
+}
+
+int Jack::getFrame() {
+    return this->frame;
+}
+
+void Jack::setFrame(int frame) {
+    this->frame = frame;
+}
+
